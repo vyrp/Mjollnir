8f4b658be18c87b194f913050ebbe1f2eac465ca
roim <roim.rodrigo@gmail.com>
2014-03-17 10:16:36 -0300

    Account management stub using Flask-Stormpath

--- bifrost/bifrost.py
+++ bifrost/bifrost.py
@@ -1,6 +1,153 @@
-from flask import Flask
-app = Flask(__name__)
+"""
+    bifrost.py
+    ~~~~~~
 
+    Bifrost, or sometimes Bilrost or Bivrost, is a burning rainbow bridge
+    that reaches between Midgard (the world) and Asgard, the realm of the gods.
+"""
+
+from os import environ
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+from flask.ext.stormpath import (
+    StormpathManager,
+    User,
+    login_required,
+    login_user,
+    logout_user,
+    user,
+)
+from stormpath.error import Error as StormpathError
+
+
+
+
+##### Initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = environ.get('STORMPATH_SECRET_KEY')
+app.config['STORMPATH_API_KEY_ID'] = environ.get('STORMPATH_API_KEY_ID')
+app.config['STORMPATH_API_KEY_SECRET'] = environ.get('STORMPATH_API_KEY_SECRET')
+app.config['STORMPATH_APPLICATION'] = environ.get('STORMPATH_APPLICATION')
+
+stormpath_manager = StormpathManager(app)
+stormpath_manager.login_view = '.login'
+
+
+
+
+##### Website
 @app.route('/')
-def root():
-    return 'Hello World!'
+def index():
+    """Basic home page."""
+    return render_template('index.html')
+
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """
+    This view allows a user to register for the site.
+
+
+    This will create a new User in Stormpath, and then log the user into their
+    new account immediately (no email verification required).
+    """
+    if request.method == 'GET':
+        return render_template('register.html')
+
+
+    try:
+        # Create a new Stormpath User.
+        _user = stormpath_manager.application.accounts.create({
+            'email': request.form.get('email'),
+            'username': request.form.get('username'),
+            'password': request.form.get('password'),
+            'given_name': 'Xupa',
+            'surname': 'Croata',
+        })
+        _user.__class__ = User
+    except StormpathError, err:
+        # If something fails, we'll display a user-friendly error message.
+        return render_template('register.html', error=err.message)
+
+
+    login_user(_user, remember=True)
+    return redirect(url_for('dashboard'))
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    This view logs in a user given an email address and password.
+
+
+    This works by querying Stormpath with the user's credentials, and either
+    getting back the User object itself, or an exception (in which case well
+    tell the user their credentials are invalid).
+
+
+    If the user is valid, we'll log them in, and store their session for later.
+    """
+    if request.method == 'GET':
+        return render_template('login.html')
+
+
+    try:
+        _user = User.from_login(
+            request.form.get('email'),
+            request.form.get('password'),
+        )
+    except StormpathError, err:
+        return render_template('login.html', error=err.message)
+
+
+    login_user(_user, remember=True)
+    return redirect(request.args.get('next') or url_for('dashboard'))
+
+
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    """
+    This view renders a simple dashboard page for logged in users.
+
+
+    Users can see their personal information on this page, as well as store
+    additional data to their account (if they so choose).
+    """
+    if request.method == 'POST':
+        if request.form.get('birthday'):
+            user.custom_data['birthday'] = request.form.get('birthday')
+
+
+        if request.form.get('color'):
+            user.custom_data['color'] = request.form.get('color')
+
+
+        user.save()
+
+
+    return render_template('dashboard.html')
+
+
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    """
+    Log out a logged in user.  Then redirect them back to the main page of the
+    site.
+    """
+    logout_user()
+    return redirect(url_for('index'))
