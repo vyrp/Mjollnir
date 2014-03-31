f051df56083ad02303112beb804917129a6d8d5c
luizmramos <luizmramos@gmail.com>
2014-03-31 17:20:13 -0300

    Adding gflags library to vigridr and using it to parse command line flags in the server.

--- vigridr/src/server/GameServer.cpp
+++ vigridr/src/server/GameServer.cpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <thread>
 
+#include <gflags/gflags.h>
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
@@ -9,6 +10,9 @@
 
 #include "GameService.h"
 #include "gen-cpp/Game.h"
+
+DEFINE_int32(port1, 9090, "Port used by the first client.");
+DEFINE_int32(port2, 9091, "Port used by the second client.");
 
 using ::apache::thrift::protocol::TBinaryProtocolFactory;
 using ::apache::thrift::protocol::TProtocolFactory;
@@ -21,21 +25,16 @@ using ::apache::thrift::transport::TTransportFactory;
 
 using namespace mjollnir::vigridr;
 
-int main(int argc, char **argv) {
-  if (argc <= 2) {
-    std::cout << "No port specified." << std::endl;
-    return 0;
-  }
-  int32_t port1, port2;
-  try {
-    port1 = std::stoi(argv[1]);
-    port2 = std::stoi(argv[2]);
-  } catch (const std::exception& e) {
-    std::cout << "Invalid port number." << std::endl;
-    return 0;
-  }
+const char* const kVersion = "v1.1";
+const char* const kUsageMessage = 
+  "This program is the server for mjollnir matches";
 
-  auto gameLogic = std::make_shared<GameLogic>(port1, port2);
+int main(int argc, char **argv) {
+  gflags::SetVersionString(kVersion);
+  gflags::SetUsageMessage(kUsageMessage);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  auto gameLogic = std::make_shared<GameLogic>(FLAGS_port1, FLAGS_port2);
   auto service = [&](int32_t port) {  
     boost::shared_ptr<GameService> handler(new GameService(gameLogic, port));
     boost::shared_ptr<TProcessor> processor(new GameProcessor(handler));
@@ -51,9 +50,8 @@ int main(int argc, char **argv) {
     server.serve();
     printf("Done");  
   };
-  printf("%d %d\n",port1, port2);
-  std::thread a(std::bind(service, port1));
-  std::thread b(std::bind(service, port2));
+  std::thread a(service, FLAGS_port1);
+  std::thread b(service, FLAGS_port2);
   a.join();
   b.join();
   
