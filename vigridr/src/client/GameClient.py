1b229e3b6a5ba7693d9aca3eead5266c03d2521c
luizmramos <luizmramos@gmail.com>
2014-05-07 17:17:32 -0300

    Changing python and cs indentation spaces from 2 to 4.

--- vigridr/src/client/GameClient.py
+++ vigridr/src/client/GameClient.py
@@ -19,78 +19,78 @@ from Command.ttypes import Coordinate
 from GameModel.ttypes import GameStatus
 
 def print_world_model(wm):
-  for i in range(3):
-    for j in range(3):
-      toPrint = ""
-      if wm.table[i][j] == Marker.X:
-        toPrint = "X"
-      elif wm.table[i][j] == Marker.O:
-        toPrint = "O"
-      else:
-        toPrint = "-"
+    for i in range(3):
+        for j in range(3):
+            toPrint = ""
+            if wm.table[i][j] == Marker.X:
+                toPrint = "X"
+            elif wm.table[i][j] == Marker.O:
+                toPrint = "O"
+            else:
+                toPrint = "-"
 
-      print toPrint + " ",
+            print toPrint + " ",
+        print
+        print
     print
-    print
-  print
 
 def play_turn(wm):
-  command = Command(Coordinate())
-  while True:
-    x = randint(0,2)
-    y = randint(0,2)
-    if wm.table[x][y] == Marker.UNMARKED:
-      command.coordinate.x = x
-      command.coordinate.y = y
-      break
-  return command
+    command = Command(Coordinate())
+    while True:
+        x = randint(0,2)
+        y = randint(0,2)
+        if wm.table[x][y] == Marker.UNMARKED:
+            command.coordinate.x = x
+            command.coordinate.y = y
+            break
+    return command
 
 
 def init():
-  pass
+    pass
 
 def synchronize(t):
-  if t>0:
-    time.sleep(t/1000.0)
+    if t>0:
+        time.sleep(t/1000.0)
 
 def play_game(client):
-  init()
-  gameInfo = client.ready()
-  start_time = time.time();
-  while True:
-    processing_time_ms = 1000*(time.time() - start_time);
-    synchronize(gameInfo.nextWorldModelTimeEstimateMs - processing_time_ms)
-    gameInfo = client.getGameInfo()
+    init()
+    gameInfo = client.ready()
     start_time = time.time();
-    wm = gameInfo.worldModel
-    print_world_model(wm)
-    if gameInfo.gameStatus == GameStatus.FINISHED:
-      break
-    if gameInfo.isMyTurn:
-      command = play_turn(wm)
-      client.sendCommand(command)
-    
+    while True:
+        processing_time_ms = 1000*(time.time() - start_time);
+        synchronize(gameInfo.nextWorldModelTimeEstimateMs - processing_time_ms)
+        gameInfo = client.getGameInfo()
+        start_time = time.time();
+        wm = gameInfo.worldModel
+        print_world_model(wm)
+        if gameInfo.gameStatus == GameStatus.FINISHED:
+            break
+        if gameInfo.isMyTurn:
+            command = play_turn(wm)
+            client.sendCommand(command)
+        
 
 def run_client():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--port", \
-                      help="Port used to connect with the server.", \
-                      type=int)
-  args = parser.parse_args()
-  if not args.port:
-    args.port = 9090
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", \
+                        help="Port used to connect with the server.", \
+                        type=int)
+    args = parser.parse_args()
+    if not args.port:
+        args.port = 9090
 
-  try:
-    transport = TSocket.TSocket('localhost', args.port)
-    transport = TTransport.TBufferedTransport(transport)
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-    client = Game.Client(protocol)
-    transport.open()
-    play_game(client)
-    transport.close()
-  except Thrift.TException, tx:
-    print 'ERROR: %s' % (tx.message)
+    try:
+        transport = TSocket.TSocket('localhost', args.port)
+        transport = TTransport.TBufferedTransport(transport)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        client = Game.Client(protocol)
+        transport.open()
+        play_game(client)
+        transport.close()
+    except Thrift.TException, tx:
+        print 'ERROR: %s' % (tx.message)
 
 
 if __name__ == "__main__":
-  run_client()
+    run_client()
