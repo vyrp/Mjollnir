dca43e79fead39579d17e39ff26146f18d40f8e9
luizmramos <luizmramos@gmail.com>
2014-05-14 08:52:04 -0300

    Change change_game_code.py so that it only copy sample clients when requested.

--- vigridr/src/change_game_code.py
+++ vigridr/src/change_game_code.py
@@ -13,7 +13,7 @@ def copy(files, dest_dir):
     if os.path.isfile(file):
       sh.copy(file, dest_dir)
 
-def change_game_code(game):
+def change_game_code(game, copy_sample_clients):
   if game not in os.listdir(games_dir):
     print 'The game "' + game + '" is not in games folder'
     return 1
@@ -22,15 +22,18 @@ def change_game_code(game):
 
   server_cpp_files = glob.glob(os.path.join(game_dir, "*.cpp"))
   server_h_files = glob.glob(os.path.join(game_dir, "*.h"))
-  client_sample_files = glob.glob(os.path.join(game_dir, "sampleclient/*"))
   thrift_files = glob.glob(os.path.join(game_dir, "*.thrift"))
 
   copy(server_cpp_files, server_dir)
   copy(server_h_files, server_dir)
   copy(thrift_files, thrifts_dir)
-  copy(client_sample_files, client_dir)
+  
+  if copy_sample_clients:
+    client_sample_files = glob.glob(os.path.join(game_dir, "sampleclient/*"))
+    copy(client_sample_files, client_dir)
+
   return 0
 
 
 if __name__ == "__main__":
-  change_game_code(sys.argv[1])
+  change_game_code(sys.argv[1], len(sys.argv)>2 and sys.argv[2]=="--with-client")
