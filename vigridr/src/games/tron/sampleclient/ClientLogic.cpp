a845f6dcc7e64c9024ea758b6025b14d8cfed680
luizmramos <luizmramos@gmail.com>
2014-05-14 12:42:36 -0300

    Adding dummy sample clients to tron

---
+++ vigridr/src/games/tron/sampleclient/ClientLogic.cpp
@@ -0,0 +1,11 @@
+#include "ClientLogic.h"
+
+using namespace ::mjollnir::vigridr;
+
+Command playTurn(const WorldModel& wm) {
+  Command command;
+  return command;
+}
+
+void init(const GameInit& gameInit) {
+}
---
+++ vigridr/src/games/tron/sampleclient/ClientLogic.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution
+{
+    public Solution(GameInit gameInit)
+    {
+    }
+
+    public Command playTurn(WorldModel wm) 
+    {
+       return null;
+    }
+}
