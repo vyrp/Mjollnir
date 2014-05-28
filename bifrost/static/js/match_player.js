6fe77ff4722c316a03f72733d0186c32fa86c470
Rodrigo Roim <rodrigo@rro.im>
2014-05-28 11:56:48 -0300

    Tic-Tac-Toe rendering logic (render_arena and render_tick)

--- bifrost/static/js/match_player.js
+++ bifrost/static/js/match_player.js
@@ -1,6 +1,12 @@
 ﻿//
-// Generic replay player. Each challenge must be have a javascript file that implements the following functions:
-// TODO
+// Generic replay player. Each challenge must have a javascript file that defines the object 'challenge_player',
+//   implementing the following functions:
+//
+// - render_arena(): renders the arena on which the match is played (e.g. the grid on a tic-tac-toe match)
+//                   this is called once right after the match json is loaded.
+//
+// - render_tick(tick): renders a specific tick (a world model) of a match (e.g. the Xs and 0s on a tic-tac-toe turn)
+//                      Keep in mind that you might need to remove elements created in the previous tick.
 //
 
 var paper = Raphael("match-player", "100%", "100%");
@@ -24,18 +30,28 @@ paper.customAttributes.arc = function (centerX, centerY, startAngle, endAngle, a
 };
 
 
-var loading_circle = paper.circle(250, 200, 25).attr({ stroke: "#f1f2f1", "stroke-width": 10 });
-var loading_circle_bar = paper.path().attr({ stroke: "#000000", "stroke-width": 10, arc: [250, 200, 0, 1, 25] });
-var loading_text = paper.text(250, 250, 'Loading').attr({ 'font-size': 20, 'fill': "#d1d2d1", 'font-family': 'Helvetica Neue", Helvetica, Arial, sans-serif', 'font-weight': 'bold' });
-var animRotation = Raphael.animation({ transform: 'r360,250,200' }, 2500).repeat(Infinity);
+
+//TODO, extract magic numbers
+var loading_circle = paper.circle(240, 200, 25).attr({ stroke: "#f1f2f1", "stroke-width": 10 });
+var loading_circle_bar = paper.path().attr({ stroke: "#000000", "stroke-width": 10, arc: [240, 200, 0, 1, 25] });
+var loading_text = paper.text(240, 250, 'Loading').attr({ 'font-size': 20, 'fill': "#d1d2d1", 'font-family': 'Helvetica Neue", Helvetica, Arial, sans-serif', 'font-weight': 'bold' });
+var animRotation = Raphael.animation({ transform: 'r360,240,200' }, 2500).repeat(Infinity);
 loading_circle_bar.animate(animRotation);
 
 
+
 $.get("https://s3-us-west-2.amazonaws.com/mjollnir-matches/" + bifrost_mid, function (data) {
-    replay_data = JSON.parse(data);
+    challenge_player.match_data = JSON.parse(data);
 
     loading_circle.remove();
     loading_circle_bar.remove();
     loading_text.remove();
 
+    challenge_player.current_tick = 0;
+    challenge_player.total_ticks = challenge_player.match_data.wmList.length;
+
+    challenge_player.render_arena();
+    challenge_player.render_tick(challenge_player.current_tick);
+
+    // The other ticks are rendered using the buttons on match.html
 })
--- bifrost/static/js/tictactoe.js
+++ bifrost/static/js/tictactoe.js
@@ -1,1 +1,35 @@
-﻿
+﻿//
+// Tic-Tac-Toe (61dd3230-2ea1-4cc1-b521-457f91b03a9e)
+//
+challenge_player = {}
+
+challenge_player.render_arena = function() {
+    // The tic-tac-toe grid is 300x300, on a 480x400 canvas
+    var arena = paper.path("M190,70 L190,370 M290,70 L290,370 M90,170 L390,170 M90,270 L390,270").attr({ stroke: "#aaaaaa", 'stroke-width': 5 });
+}
+
+var drawn_elements = [];
+var font_attrs = { 'font-size': 70, 'fill': "#d1d2d1", 'font-family': 'Helvetica Neue", Helvetica, Arial, sans-serif', 'font-weight': 'bold' };
+challenge_player.render_tick = function (tick) {
+
+    // Cleanup
+    drawn_elements.forEach(function (e) {
+        e.remove();
+    })
+
+    drawn_elements = [];
+
+    // Render logic
+    var WM = challenge_player.match_data.wmList[tick];
+
+    for (row in WM.table) {
+        for (column in WM.table[row]) {
+            var mark = WM.table[row][column];
+
+            if (mark == 'X' || mark == 'O') {
+                drawn_elements.push( paper.text(140 + column*100, 120 + row*100, mark).attr(font_attrs) );
+            }
+        }
+    }
+
+}
