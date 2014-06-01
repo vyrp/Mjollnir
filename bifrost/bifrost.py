d6b5801847495cd163e07aa2d9e586a4973eb972
Rodrigo Roim Ferreira <rodrigo@rro.im>
2014-06-01 16:04:16 -0300

    Rounding percentile on /user/<uid>

--- bifrost/bifrost.py
+++ bifrost/bifrost.py
@@ -293,7 +293,7 @@ def user_page(username):
         submission['name'] = challenge['name']
         submission['RD'] = round(submission['RD'], 2)
         submission['rank'] = mongodb.submissions.find({ 'cid': submission['cid'], 'rating': { '$gt': submission['rating'] } }).count() + 1
-        submission['percentile'] = 100*float(total_submissions - submission['rank'] + 1)/total_submissions
+        submission['percentile'] = round(100*float(total_submissions - submission['rank'] + 1)/total_submissions, 2)
         challenge_solutions.append(submission)
 
     return render_template('dashboard.html', user_in_db = user_in_db, challenge_solutions = challenge_solutions, custom_title = username)
